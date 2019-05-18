808da7c1aa271c312a7b0f960997f6e8ac7edfc8
Jovan Stojanovic <yole71@gmail.com>
2019-05-18 22:41:30 +0200

    Update screen-stratum.sh

--- screen-stratum.sh
+++ screen-stratum.sh
@@ -8,6 +8,6 @@
  #screen -dmS quark $STRATUM_DIR/run.sh quark
  #screen -dmS scrypt $STRATUM_DIR/run.sh scrypt
  #screen -dmS skein $STRATUM_DIR/run.sh skein
- screen -dmS x166 $STRATUM_DIR/run.sh x16r
+ screen -dmS x16r $STRATUM_DIR/run.sh x16r
  #screen -dmS xevan $STRATUM_DIR/run.sh xevan
  
